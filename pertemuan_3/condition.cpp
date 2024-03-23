25ec3562487203ad3b4364be2f9ce7a456b1405a
goncang1512 <samuderanst@gmail.com>
2024-03-23 11:56:50 +0700

    Pertemuan 3: Condition If statement and Switch

---
+++ pertemuan_3/condition.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int nilai;
+
+    cout << "Masukkan nilai : ";
+    cin >> nilai;
+
+    // if statement
+    // if(nilai < 65) {
+    //     cout << "Tidak lulus" << endl;
+    // } 
+
+    // if else statement
+    // if(nilai < 65) {
+    //     cout << "Tidak lulus" << endl;
+    // } else {
+    //     cout << "Lulus" << endl;
+    // } 
+
+    // If-Else-If statement
+    // if(nilai < 65) {
+    //     cout << "Tidak lulus" << endl;
+    // } else if(nilai > 65) {
+    //     cout << "Lulus" << endl;
+    // } else if(nilai == 65) {
+    //     cout << "Lulus dengan nilai pas" << endl;
+    // }
+
+    //Nested If
+    // if(nilai < 0 || nilai > 100) {
+    //     cout << "Tidak valid";
+    // } else {
+    //     if(nilai < 65) {
+    //     cout << "Tidak lulus" << endl;
+    //     } else if(nilai > 65) {
+    //         cout << "Lulus" << endl;
+    //     } else if(nilai == 65) {
+    //         cout << "Lulus dengan nilai pas" << endl;
+    //     }
+    // }
+
+    // Switch 
+    // switch (nilai) {
+    //     case 1: 
+    //         cout << "Senin" << endl;
+    //         break;
+    //     case 2: 
+    //         cout << "Selasa" << endl;
+    //         break;
+    //     case 3: 
+    //         cout << "Rabu" << endl;
+    //         break;
+    //     case 4: 
+    //         cout << "Kamis" << endl;
+    //         break;
+    //     case 5: 
+    //         cout << "Jumat" << endl;
+    //         break;
+    //     case 6: 
+    //         cout << "Sabtu" << endl;
+    //         break;
+    //     case 7: 
+    //         cout << "Minggu" << endl;
+    //         break;
+    //     default: 
+    //         cout << "Tidak valid" << endl;
+    //         break;
+    // }
+
+    // Switch case for range
+    // switch (nilai) {
+    //     case 90 ... 100:
+    //         cout << "A" << endl;
+    //         break;
+    //     case 85 ... 89:
+    //         cout << "B+" << endl;
+    //         break;
+    //     case 80 ... 84:
+    //         cout << "B" << endl;
+    //         break;
+    //     case 75 ... 79:
+    //         cout << "C+" << endl;
+    //         break;
+    //     case 70 ... 74:
+    //         cout << "C" << endl;
+    //         break;
+    //     case 65 ... 69:
+    //         cout << "D" << endl;
+    //         break;
+    //     case 0 ... 64: 
+    //         cout << "E" << endl;
+    //         break;
+    //     default:
+    //         cout << "Tidak valid" << endl;
+    // }
+
+    // Ternary operator (Short Hand If-Else)
+    // string tipe = (nilai > 0) ? "Positif" : "Negatif";
+    // cout << tipe << endl;
+
+}
